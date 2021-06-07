bbacd367beb33534796329795103e7498b044ab4
github-mxf <ma.xf@163.com>
2021-06-07 10:59:18 +0800

    dev test2

--- __test__/api/search.spec.js
+++ __test__/api/search.spec.js
@@ -1,13 +1,13 @@
-// import { getHotSearchKeyword } from 'api/search';
-// import { HOT_WORD } from 'api/config'
+import { getHotSearchKeyword } from 'api/search';
+import { HOT_WORD } from 'api/config'
 
-// describe('api-search', () => {
-//   test('getHotSearchKeyword', async () => {
-//     try {
-//       const result = await getHotSearchKeyword();
-//       expect(result).toBe(HOT_WORD);
-//     } catch (e) {
-//       console.log('getHotSearchKeyword error:' + e);
-//     }
-//   })
-// })
+describe('api-search', () => {
+  test('getHotSearchKeyword', async () => {
+    try {
+      const result = await getHotSearchKeyword();
+      expect(result).toBe(HOT_WORD);
+    } catch (e) {
+      console.log('getHotSearchKeyword error:' + e);
+    }
+  })
+})
